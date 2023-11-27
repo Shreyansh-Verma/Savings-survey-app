9719e07c77b84d9dfd7f1edc5758389b8b325094
Shreyansh-Verma <shreyansh.verma245@gmail.com>
2023-11-27 20:46:59 +0530

    Fix intro question

--- frontend/savings-app/src/components/IntroQuestions.js
+++ frontend/savings-app/src/components/IntroQuestions.js
@@ -4,7 +4,7 @@ import './IntroQuestions.css';
 
 const IntroQuestions = ({ handleResponse }) => {
   const introQuestions = [
-    { question: 'Age', options: ['Less than 18 years', '18-30 years', 'Above 30 years'] },
+    { question: 'Age', options: ['Less than 18 years', 'Between 18-30 years', 'Above 30 years'] },
     { question: 'Gender', options: ['Male', 'Female'] },
     { question: 'Monthly Income Range', options: ['Below Rs 10,000', 'Between Rs 10,000 - Rs 50,000', 'Between Rs 50,000 - Rs 100,000', 'Above Rs 100,000'] },
     { question: 'Education Level', options: ['No education', 'High School Graduate', 'Bachelors Degree', 'Masters Degree'] },
